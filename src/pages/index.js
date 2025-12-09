dec0dda9c48f5d1ff1a98a3d37bcf7a38a2ec55b
livia <149427626+livisaraujo@users.noreply.github.com>
2025-12-09 15:08:22 -0400

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,7 +2,7 @@ import { Container, Row } from "react-bootstrap";
 import Cards from "./components/cards"
 
 export async function getServerSideProps() {
-    const res = await fetch('https://fw2api-xi.vercel.app/noticias');
+    const res = await fetch('https://apilivia.vercel.app/noticias');
     const repo = await res.json(res);
     return { props: { noticias: repo } }
 }
